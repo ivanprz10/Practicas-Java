428da1b59e22f153e4903092dd20a44633cde93a
ivanprz10 <ivanpcruz07@gmail.com>
2024-08-07 13:29:39 -0600

    Metodos de orodenamiendo insercion

---
+++ MetodosOrdenamiento/Metodo_insercion/src/main/java/com/mycompany/metodo_insercion/Metodo_insercion.java
@@ -0,0 +1,31 @@
+package com.mycompany.metodo_insercion;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author ivanp
+ */
+public class Metodo_insercion {
+
+    public static void main(String[] args) {
+
+        int[] arr = { 9, 5, 1, 4, 3 };
+        System.out.println("Arreglo desordenado: "+Arrays.toString(arr));
+        
+        for (int i = 0; i < arr.length; i++) {
+            int key=arr[i];
+            int j=i-1;
+            while (j >= 0 && key < arr[j]) { 
+                
+                arr[j+1]=arr[j];
+                --j;  
+            }
+            // Coloca a key después del elemento más pequeño que este
+            arr[j + 1] = key;
+        }
+                System.out.println("Arreglo ordenado: "+Arrays.toString(arr));
+
+
+    }
+}
